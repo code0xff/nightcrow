e441584d53ae988f24f1741bd72b0371cce43a92
whackur <whackur@gmail.com>
2026-08-04 01:16:15 +0900

    chore(viewer): rebuild dist for the current sources

--- viewer-ui/dist/assets/Html-zHO_NJ4V.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CVyd7CZ2.js";function i({source:e}){return r.jsx("iframe",{title:"HTML preview",sandbox:"",srcDoc:e,className:"h-full w-full border-0 bg-white"})}export{i as HtmlView};
